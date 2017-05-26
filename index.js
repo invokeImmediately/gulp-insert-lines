89e6ebebf2aa49d36942ea3e3a5f13107246ed9b
Daniel Rieck <danielcrieck@gmail.com>
2017-05-26 11:10:19 -0700

    Adding capability to stop matching after first hit

--- index.js
+++ index.js
@@ -6,13 +6,27 @@ module.exports = function (opt) {
     if (file.isNull()) return this.emit('data', file);
     if (file.isStream()) return this.emit('error', new Error("gulp-insert-lines: Streaming not supported"));
     var str = file.contents.toString('utf8');
-    var line, i, lines;
-    var newLines = [];
-
+    var line, i, lines, newLines;
+    
     lines = str.split(/\r\n|\r|\n/g);
-
+    
+    if (!opt.stopMatching) {
+      newLines = matchOnAllLines(lines, opt);
+    } else {
+      newLines = matchUntilFirstHit(lines, opt);		
+    }
+    
+    str = newLines.join('\n');
+    
+    file.contents = new Buffer(str);
+    this.emit('data', file);
+  }
+  
+  function matchOnAllLines(lines, opt) {
+    var newLines = [];
+    
     for (i = 0; i < lines.length; i++) {
-
+      
       if (opt.after) {
         if (lines[i].match(opt.after)) {
           newLines.push(lines[i]);
@@ -21,7 +35,7 @@ module.exports = function (opt) {
           newLines.push(lines[i]);
         }
       }
-
+      
       if (opt.before) {
         if (lines[i].match(opt.before)) {
           newLines.push(opt.lineBefore);
@@ -31,12 +45,42 @@ module.exports = function (opt) {
         }
       }
     }
-
-    str = newLines.join('\n');
-
-    file.contents = new Buffer(str);
-    this.emit('data', file);
+    
+    return newLines;
   }
-
+  
+  function matchUntilFirstHit(lines, opt) {
+    var newLines = [],
+      matchFound = false;
+    
+    for (i = 0; i < lines.length; i++) {
+      if (!matchFound) {
+        if (opt.after) {
+          if (lines[i].match(opt.after)) {
+            matchFound = true;
+            newLines.push(lines[i]);
+            newLines.push(opt.lineAfter);
+          } else {
+            newLines.push(lines[i]);
+          }
+        }
+        
+        if (opt.before) {
+          if (lines[i].match(opt.before)) {
+            matchFound = true;
+            newLines.push(opt.lineBefore);
+            newLines.push(lines[i]);
+          } else {
+            newLines.push(lines[i]);
+          }
+        }
+      } else {
+        newLines.push(lines[i]);
+      }
+    }
+    
+    return newLines;
+  }
+  
   return es.through(gulpInsertLines);
 };
